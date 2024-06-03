5675605fe3d6113e4771a5b094e5a10bb82fa97e
BigHuni <hdh8659@gmail.com>
2024-06-03 16:18:46 +0900

    [ADD] 날씨 일기 수정 API 코드 추가

--- src/main/java/Meohun/weather/repository/DiaryRepository.java
+++ src/main/java/Meohun/weather/repository/DiaryRepository.java
@@ -12,6 +12,8 @@ public interface DiaryRepository extends JpaRepository<Diary, Integer> {
     List<Diary> findAllByDate(LocalDate date);
 
     List<Diary> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
+
+    Diary getFirstByDate(LocalDate date);
 }
 
 
