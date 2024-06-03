8bd17f1edf59a1f7e67e05d55d292ca1ef9f7924
BigHuni <hdh8659@gmail.com>
2024-06-03 16:26:02 +0900

    [ADD] 날씨 일기 삭제 API 코드 추가

--- src/main/java/Meohun/weather/repository/DiaryRepository.java
+++ src/main/java/Meohun/weather/repository/DiaryRepository.java
@@ -3,6 +3,7 @@ package Meohun.weather.repository;
 import Meohun.weather.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,6 +15,9 @@ public interface DiaryRepository extends JpaRepository<Diary, Integer> {
     List<Diary> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
 
     Diary getFirstByDate(LocalDate date);
+
+    @Transactional
+    void deleteAllByDate(LocalDate date);
 }
 
 
